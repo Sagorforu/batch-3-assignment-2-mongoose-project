47962373f03d36876d5598f7d064f99d13c479df
Md Sagor Islam <sagorislam1318@gmail.com>
2024-05-22 16:49:31 +0100

    build script fix

--- dist/app.js
+++ dist/app.js
@@ -4,9 +4,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const products_route_1 = require("./modules/products/products.route");
+const orders_route_1 = require("./modules/orders/orders.route");
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
-const port = 3000;
-app.get("/", (req, res) => {
-    res.send("This is project mongoose - assignment 2");
+//!parsers
+app.use(express_1.default.json());
+app.use((0, cors_1.default)());
+app.use('/api/products', products_route_1.ProductRoutes);
+app.use('/api/orders', orders_route_1.OrderRoutes);
+app.get('/', (req, res) => {
+    res.send('This is project mongoose - assignment 2');
 });
 exports.default = app;
